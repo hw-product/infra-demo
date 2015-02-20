b2ef60af3bac15db4bc195489672365238928470
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-20 13:17:27 -0800

    Disable application stacks

--- cloudformation/infrastructure.rb
+++ cloudformation/infrastructure.rb
@@ -1,6 +1,6 @@
 SparkleFormation.new(:infrastructure) do
   nest!(:infrastructure__chef_server)
   nest!(:infrastructure__gemstore)
-  nest!(:applications__wemux)
-  nest!(:applications__yetty)
+  # nest!(:applications__wemux)
+  # nest!(:applications__yetty)
 end
